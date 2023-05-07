008c89cf4b07e1b0fbe22623d30f183f1deab831
gnl <gnl0@qq.com>
2023-05-07 11:26:35 +0800

    update

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="header">
+      <h1 class="m-3 object-none object-left-top text-3xl font-bold">
+          pure-pltf-web
+      </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,11 +1,42 @@
 <script setup>
+	const getSysInfo = () => {
+		console.log('getSysInfo()')
 
+		// fetch("", {})
+		// 		.then(res => {})
+		// 		.catch(err => {
+		// 			console.log('getSysInfo() error ', err.message)
+		// 		})
+	}
+
+	const selectToInstall = () => {
+		console.log('selectToInstall()')
+	}
+
+	const checkForUpdate = () => {
+		console.log('checkForUpdate()')
+	}
+
+	const selectToUninstall = () => {
+		console.log('selectToUninstall()')
+	}
 </script>
 
 <template>
-  <h1>this is dashboard</h1>
+  <div id="dashboard" class="m-5 p-3 min-h-full h-96 rounded-xl flex">
+	  <div class="control bg-indigo-500 hover:bg-indigo-800" @click="getSysInfo">SysInfo</div>
+	  <div class="control bg-green-600 hover:bg-green-800" @click="selectToInstall">Install</div>
+		<div class="control bg-yellow-400 hover:bg-yellow-600" @click="checkForUpdate">Upgrade</div>
+	  <div class="control bg-red-500 hover:bg-red-900" @click="selectToUninstall">Uninstall</div>
+  </div>
 </template>
 
 <style scoped>
+#dashboard {
+  background-color: white;
+}
 
+.control {
+	@apply w-96 m-3 flex justify-center items-center rounded-xl text-white text-xl font-medium
+}
 </style>
